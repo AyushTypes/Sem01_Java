4b67e69c50de0589f22c9352d25f456adb6f8541
Ayush Prasad Shrestha <np01ai4a250123@islingtoncollege.edu.np>
2025-11-20 12:52:26 +0545

    Workshop Week 3 projects

---
+++ JustTrying/Medical_Inventory.java
@@ -0,0 +1,34 @@
+package JustTrying;
+import java.util.Scanner;
+/**
+ * Write a description of class Medical_Inventory here.
+ *
+ * @author (Ayush P.)
+ * @version (1.2)
+ */
+public class Medical_Inventory
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Medical_Inventory
+     */
+    public Medical_Inventory()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
---
+++ Week03/Wk03_W_1.java
@@ -0,0 +1,15 @@
+package Week03;
+
+
+/**
+ * Write a description of class Wk03_W_1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Wk03_W_1
+{
+    public static void main(String[]arg){
+        system.out.println("Pushing in Git");
+    }
+}
